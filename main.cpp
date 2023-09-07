ae3031d0c1543b2680bcf048b0d7f2654fefc739
SaaFomenko <s.fomenko@list.ru>
2023-09-07 20:50:21 +0300

    Need fix concurrency.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <atomic>
 #include <chrono>
+#include <unistd.h>
 #include <iostream>
 #include <mutex>
 #include <thread>
@@ -10,48 +11,65 @@ using namespace std::chrono_literals;
 namespace my
 {
     std::mutex m;
-    std::atomic<char> p{0};
+    //std::mutex l;
+    //std::atomic<int> thr{0};
+    //std::atomic<std::thread::id> thr_id;
+    //std::atomic<std::chrono::duration<double>> elapsed_seconds;
+    std::atomic<char> Spc{' '};
+    //std::atomic<char> Non{0};
+    //std::atomic<char> End{'\n'};
+    //std::atomic<char> Tab{'\t'};
 
     void print_progress(int i, int n)
     {
         const int bar_size = 20;
+        //thr.store(i);
+        //thr_id.store(std::this_thread::get_id());
         Color::Modifier green(Color::BG_GREEN);
         Color::Modifier def(Color::BG_DEFAULT);
 
-        //std::lock_guard<std::mutex> lock_trh(m);
-
-        m.lock();
+        std::lock_guard<std::mutex> lock_trh(m);
+        //std::unique_lock<std::mutex> lm(m);
 
         if (i == 0)
         {
             std::cout << "N\tid\t\tProgress Bar\t\tTime\n";
         }
 
-
         std::cout << i << '\t' << std::this_thread::get_id() << '\t';
-            
-        m.unlock();
+        //std::cout << thr.load() << '\t' << thr_id.load() << '\t';
+
+        //lock_trh.~lock_guard();
 
         auto start = std::chrono::steady_clock::now();
-        for(int i = 0; i < bar_size; ++i)
-        {
-            p.store(' ');
-            m.lock();
-            std::cout << green << p.load();
-            m.unlock();
-            std::this_thread::sleep_for(0.3s);
-        }
+        //for(int i = 0; i < n; ++i)
+        //{
+            for (int j = 0; j < bar_size; ++j)
+            {
+                //p.store(' ', std::memory_order_relaxed);
+                //std::lock_guard<std::mutex> lock_trh(l);
+                std::cout.setf(std::ios::unitbuf);
+                std::cout << green << Spc.load(std::memory_order_relaxed);
+                //std::cout << green << ' ';
+                //sleep(100);
+                std::this_thread::sleep_for(0.2s);
+            }
+        //}
         auto end = std::chrono::steady_clock::now();
         std::chrono::duration<double> elapsed_seconds = end - start;
-        m.lock();
+
         std::cout << def << '\t' << elapsed_seconds.count() << "s\n";
-        m.unlock();
+        //std::lock(lm, ll);
     }
 }
 
 int main()
 {
+    std::ios_base::sync_with_stdio(false);
     int n = 5;
+    // int i = 0;
+
+    //my::print_progress(i, n);
 
     std::thread* thr_arr = new std::thread[n];
 
